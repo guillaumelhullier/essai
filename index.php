89bfb92bc61a95d2ba5f5f1ea94797b8a994b559
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-03-03 17:36:16 +0100

    echo

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "bonjour tout le monde";
-echo "Je vais bien";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+echo "demain" ;
+echo "train";
+?>
+</body>
+
+</html>
