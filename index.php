6f0c94543f7c79845860235a70b3f51fa83848fd
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-03-05 14:43:21 +0100

    index

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
 </head>
 
 <body>
+    <h1>Coucou</h1>
     <?php
+    $ville = "Lyon";
+    $department = "Rhône";
+
+    $addition = 'La ville de ' .$ville. ' se trouve dans le département du ' .$department ;
 //echo "demain <br/> ";
-echo "train";
+echo $addition;
 ?>
 </body>
 
