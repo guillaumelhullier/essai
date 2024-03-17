f3fb6008570aead67ba5e31fda4815a6a7825b4e
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-03-17 17:39:13 +0100

    sass

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js" defer></script>
+    <!-- <script src="jquery.js" async></script>-->
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
 
-/*# sourceMappingURL=index.php.map */
+<body>
+    <h1>Coucou les gens<h1>
+            <?php
+    echo "Demain est un autre jour";
+    ?>
+</body>
+
+</html>
