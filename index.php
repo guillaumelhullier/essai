b2278ee7f03d9b3cb29291bab9003e5081e30cdf
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-03-17 16:25:11 +0100

    sum

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="script.js" defer></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <link rel="stylesheet" href="style. scss">
-    <title>Document</title>
-</head>
 
-<body>
-    <h1>Coucou</h1>
-    <?php
-    $ville = "Lyon";
-    $department = "Rhône";
-    $addition = 'La ville de ' .$ville. ' se trouve dans le département du ' .$department.'<br/>' ;
-//echo "demain <br/> ";
-echo $addition;
-echo "return True";
-?>
-</body>
-
-</html>
+/*# sourceMappingURL=index.php.map */
