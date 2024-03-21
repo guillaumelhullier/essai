586df9f1ee951d499d3ac2eff60318eef1864aaa
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-03-21 16:45:37 +0100

    puissance

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 /**
- * Fonction identifiant premier élément du tableau
- * @param {array} table
+ * Fonction calculateur de puissance
+ * @param {nombre} voltage
+ * @param {nombre} current
  * @returns
  */
 
-function getFirstValue(table) {
-  return table[0];
+function circuitPower(voltage, current) {
+  return voltage * current;
 }
 
-console.log(getFirstValue([1, 2, 3]));
-console.log(getFirstValue([80, 5, 100]));
-console.log(getFirstValue([-500, 0, 50]));
+console.log(circuitPower(230, 10));
+console.log(circuitPower(110, 3));
+console.log(circuitPower(480, 20));
