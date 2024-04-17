563fb1326d12d6bc01a1b627c85f72a6608637c1
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-04-17 18:44:57 +0200

    script

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
     <script src="javascript.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -11,8 +11,8 @@
 
 <body>
     <h1>Bonjour à tous</h1>
-
-    <?php
+    <p>
+        <?php
     
 function triArea ($a,$b){
     return $a*$b/2;
@@ -23,6 +23,7 @@ echo triArea(7,4).'</br>';
 echo triArea(10,10).'</br>';
 
     ?>
+    </p>
 </body>
 
 </html>
